f3b14f7c2b3f970ebba443ba3f5a6e1596f4533e
Emir Can Özcan <118878432+Hoschel@users.noreply.github.com>
2026-08-25 22:29:20 +0300

    feat(sync): expose file patch endpoint for editor concurrency

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,24 +1,19 @@
 import { Router } from "express";
-import { createProject, deleteProject, getProject, getPublicProject, listProjects, publishProject, updateProjectFiles } from "../controllers/projectController.js";
+import { createProject, deleteProject, getProject, getPublicProject, listProjects, publishProject, updateProjectFiles, patchProjectFiles } from "../controllers/projectController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { chat } from "../controllers/chatController.js";
 
 const projectRouter = Router();
 
-// Public Route
-projectRouter.get("/public/:id", getPublicProject)
-
-// Protect all following routes
-projectRouter.use(authMiddleware)
-
-projectRouter.post("/", createProject)
-projectRouter.get("/", listProjects)
-projectRouter.get("/:id", getProject)
-projectRouter.delete("/:id", deleteProject)
-projectRouter.put("/:id/files", updateProjectFiles)
-projectRouter.post("/:id/publish", publishProject)
-
-// Chat
-projectRouter.post("/:id/chat", chat)
+projectRouter.get("/public/:id", getPublicProject);
+projectRouter.use(authMiddleware);
+projectRouter.post("/", createProject);
+projectRouter.get("/", listProjects);
+projectRouter.get("/:id", getProject);
+projectRouter.delete("/:id", deleteProject);
+projectRouter.put("/:id/files", updateProjectFiles);
+projectRouter.patch("/:id/files", patchProjectFiles);
+projectRouter.post("/:id/publish", publishProject);
+projectRouter.post("/:id/chat", chat);
 
 export default projectRouter;
